1f0e1bc337053c7538c1df7a629363c7b53ada99
Сергеенков Михаил <sergeenkov.mihail@gmail.com>
2017-03-28 11:21:51 +0300

    Письма

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,21 @@ class Ticket < ApplicationRecord
   belongs_to :user
 
   scope :find_tickets, ->(user) { user.admin? ? all : where(user: user) }
+
+  after_create :send_notification_about_buy_ticket
+  after_destroy :send_notification_about_delete_ticket
+
+  def route_name
+    "#{start_station.title} - #{finish_station.title}"
+  end
+
+  private
+
+  def send_notification_about_buy_ticket
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
+  end
+
+  def send_notification_about_delete_ticket
+    TicketsMailer.delete_ticket(self.user, self).deliver_now
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326144211) do
+ActiveRecord::Schema.define(version: 20170328072622) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer  "train_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170326144211) do
     t.integer  "seats"
     t.integer  "number"
     t.integer  "position"
+    t.index ["id", "type"], name: "index_carriages_on_id_and_type"
     t.index ["train_id"], name: "index_carriages_on_train_id"
   end
 
